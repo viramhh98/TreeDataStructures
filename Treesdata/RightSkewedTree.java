5cfb8ed9e80c55e3c78b840e12fd938cc5b8975f
viram shah <shahviramhh98@gmail.com>
2023-10-13 19:08:14 +0530

    right skewed tree added to the project

---
+++ Treesdata/RightSkewedTree.java
@@ -0,0 +1,128 @@
+package Treesdata;
+
+import java.util.Scanner;
+
+class RightSkewedTree {
+    public static void main(String[] args) {
+        // insert the values in binary search tree and then it will convert to right skewed tree
+
+        treeObj1 bst = new treeObj1();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number of nodes you want to enter : ");
+        int NodeCount = sc.nextInt();
+        System.out.println();
+        for (int i = 0; i < NodeCount; i++) {
+            System.out.print("enter the node value : ");
+            int val = sc.nextInt();
+            bst.addNode(val);
+            System.out.println();
+        }
+
+        System.out.println("\t\tbinary search tree\n");
+        bst.visualizeTree(bst.head, 0);
+        bst.inorder(bst.head);
+        System.out.println("\n\n\n");
+        System.out.println("\t\tleft skewed tree\n");
+        bst.printLevel();
+        bst.visualizeTree(bst.headRightSkewedTree, 0);
+    }
+}
+
+class treeObj1 {
+    Node head;
+    Node headRightSkewedTree;
+
+    class Node {
+        Node leftChild;
+        Node rightChild;
+        Node parent;
+        int data;
+
+        Node(int data) {
+            leftChild = null;
+            rightChild = null;
+            this.data = data;
+        }
+    }
+
+    void addNode(int data) {
+        if (head == null) {
+            Node n = new Node(data);
+            head = n;
+        } else {
+            Node traverse = head;
+            Node n = new Node(data);
+            Node beforeNull = null;
+            while (traverse != null) {
+                beforeNull = traverse;
+                if (traverse.data > data) {
+                    traverse = traverse.leftChild;
+                } else {
+                    traverse = traverse.rightChild;
+                }
+            }
+            if (beforeNull.data > data) {
+                beforeNull.leftChild = n;
+                n.parent = beforeNull;
+            } else {
+                beforeNull.rightChild = n;
+                n.parent = beforeNull;
+            }
+        }
+
+    }
+
+    void inorder(Node n) {
+
+        if (n == null) {
+            return;
+        }
+        inorder(n.leftChild);
+        InsertLeftSkewedTree(n.data);
+        inorder(n.rightChild);
+    }
+
+    void InsertLeftSkewedTree(int data) {
+        if (headRightSkewedTree == null) {
+            Node n = new Node(data);
+            headRightSkewedTree = n;
+        } else {
+            Node traverse = headRightSkewedTree;
+            Node n = new Node(data);
+            while (traverse.rightChild != null) {
+                traverse = traverse.rightChild;
+            }
+            traverse.rightChild = n;
+        }
+
+    }
+
+    void printLevel() {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("0" + i + "  ");
+        }
+        System.out.println("   levels");
+        System.out.println();
+    }
+
+    void visualizeTree(Node node, int level) {
+        Node traverse = node;
+        if (traverse == null) {
+            return;
+        }
+
+        visualizeTree(traverse.rightChild, level + 1);
+
+        for (int i = 0; i < level; i++) {
+            System.out.print("    ");
+        }
+        if (traverse.data < 10) {
+            System.out.println("0" + traverse.data);
+        } else {
+            System.out.println(traverse.data);
+        }
+
+        visualizeTree(traverse.leftChild, level + 1);
+    }
+}
